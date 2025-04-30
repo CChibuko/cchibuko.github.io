68e29d749ff18b05875b77caa3a7d2bbdb073057
Chioma Chibuko <cchibuko@csustan.edu>
2025-04-30 09:40:31 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,20 +33,20 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Tendon Morphology in Lizards',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Poster presentation on research looking at how muscles and tendons work as a unit in locomotion',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://github.com/CChibuko/cchibuko.github.io/blob/main/Screenshot_30-4-2025_93542_.jpeg',
+          link: 'https://github.com/CChibuko/cchibuko.github.io/blob/main/Tendon%20Morphology%20in%20Lizards.pdf',
         },
         {
-          title: 'Project Name',
+          title: 'Ecology Lab Poster',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Poster presentation on a paper about West Nile virus and how its vectors spread it',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://github.com/CChibuko/cchibuko.github.io/blob/main/Screenshot_30-4-2025_93857_.jpeg',
+          link: 'https://github.com/CChibuko/cchibuko.github.io/blob/main/Ecology%20Lab%20Poster%20Presentation.pdf',
         },
       ],
     },
