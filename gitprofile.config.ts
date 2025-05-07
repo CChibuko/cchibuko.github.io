937615a64515e7e41af89b7461f3ee752f722bff
Chioma Chibuko <cchibuko@csustan.edu>
2025-05-07 09:24:34 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -80,7 +80,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/CChibuko/cchibuko.github.io/raw/main/ChiomaChibukoCV.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Agarose Gel',
